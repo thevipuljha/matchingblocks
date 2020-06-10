a6b1e51f8fa156b4deeb5e78ef5a2dd3ebbabc23
Vipul Jha <vipuljha1142@gmail.com>
2020-06-10 16:18:22 +0530

    added theme selectors

--- javascript.js
+++ javascript.js
@@ -2,6 +2,16 @@ const elementById = (id) => document.getElementById(id);
 const elementsByclass = (className) => document.getElementsByClassName(className);
 const themeList = ["Default", "Ben", "Cartoons", "DC", "Marvel", "Pokemon", "Programming", "TechBrands"];
 
+function setThemePics() {
+    const themeButtons = elementsByclass("theme-pic");
+    for (let index = 0; index < themeButtons.length; index++) {
+        const theme = themeButtons[index].id;
+        const imagepath = `url(images/${theme}/image0.png)`
+        themeButtons[index].style.backgroundImage = imagepath;
+    }
+}
+
+
 const createBlocks = () => {
     const blocksContainer = elementById("blocksContainer");
     for (let index = 0; index < 20; index++) {
@@ -11,10 +21,11 @@ const createBlocks = () => {
         newBlock.style.backgroundImage = 'url("images/Default/image0.png")';
         blocksContainer.appendChild(newBlock);
     }
-}
+};
 // initial function to run on page load
 const initiate = () => {
     createBlocks();
+    setThemePics();
 };
 
 window.onload = initiate;
