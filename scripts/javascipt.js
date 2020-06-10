579697173ce2fa56b7db08492a88173f9801c098
Vipul Jha <vipuljha1142@gmail.com>
2020-06-10 13:09:52 +0530

    initial files added

---
+++ scripts/javascipt.js
@@ -0,0 +1,9 @@
+const elementById = (id) => document.getElementById(id);
+const elementsByclass = (className) => document.getElementsByClassName(className);
+
+// initial function to run on page load
+const initiate = () => {
+
+};
+
+window.onload = initiate;
